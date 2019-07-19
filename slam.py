66c0bfb5d1070417cf3f0e5a24108aec2e2aeca0
Nguyen Van Thanh <thanhnv@deha-soft.com>
2019-07-19 17:01:27 +0700

    add video_test

--- slam.py
+++ slam.py
@@ -1,8 +1,20 @@
 import cv2
+import pygame
+import time
+
+W = 960
+H = 540
+pygame.init()
+display = pygame.display.set_mode((W,H))
 
 def process_frame(img):
-	img = cv2.resize(img, (540, 960))
-	print(img.shape)
+	img = cv2.resize(img, (960, 540))
+	surf = pygame.surfarray.make_surface(img.swapaxes(0,1)).convert()
+
+	display.blit(surf, (0, 0))
+	pygame.display.update()
+	#time.sleep(1)
+
 
 if __name__ == '__main__':
 	cap = cv2.VideoCapture("test.mp4")
