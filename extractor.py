017b61af190b6bc56d3298fad506feb6c1fbd62a
Nguyen Van Thanh <thanhnv@deha-soft.com>
2019-10-31 14:53:46 +0700

    extract feature

---
+++ extractor.py
@@ -0,0 +1,26 @@
+import cv2
+import numpy as np
+
+class Extractor(object):
+	GX = 16//2
+	GY = 16//2
+	def __init__(self):
+		self.orb = cv2.ORB_create(100)
+		self.bf = cv2.BFMatcher()
+		self.last = None
+
+	def extract(self, img):
+		#detection
+		feats = cv2.goodFeaturesToTrack(np.mean(img, axis = 2).astype(np.uint8), 3000, qualityLevel = 0.01, minDistance = 3)
+		
+		# extraction
+		kps = [cv2.KeyPoint(x = f[0][0], y = f[0][1], _size = 20) for f in feats]
+		kps, des = self.orb.compute(img, kps)
+
+		# matching
+		matches = None
+		self.last = {'kps': kps, 'des': des}
+		if self.last is not None:
+			matches = self.bf.match(des, self.last['des'])
+
+		return kps, des, matches
--- slam.py
+++ slam.py
@@ -1,20 +1,26 @@
 import cv2
-import pygame
 import time
+import numpy as np
+from extractor import Extractor
 
 W = 960
 H = 540
-pygame.init()
-display = pygame.display.set_mode((W,H))
 
+fe = Extractor()
 def process_frame(img):
 	img = cv2.resize(img, (960, 540))
-	surf = pygame.surfarray.make_surface(img.swapaxes(0,1)).convert()
+	# find the keypoints with ORB
+	kps, des, matches = fe.extract(img)
+	for p in kps:
+		u,v = map(lambda x: int(round(x)), p.pt)
+		cv2.circle(img, (u,v), 5, (0, 255, 0), 1)
+			
+	# for point in kp:
+	# 	u,v = map(lambda x: int(round(x)), point.pt)
+	# 	cv2.circle(img, (u,v), 5, (0, 255, 0), 1)
 
-	display.blit(surf, (0, 0))
-	pygame.display.update()
-	#time.sleep(1)
-
+	cv2.imshow("result", img)
+	cv2.waitKey(1)
 
 if __name__ == '__main__':
 	cap = cv2.VideoCapture("test.mp4")
@@ -22,5 +28,8 @@ if __name__ == '__main__':
 		ret , frame = cap.read()
 		if ret == True:
 			process_frame(frame)
+			# Press Q on keyboard to  exit
+			if cv2.waitKey(1) & 0xFF == ord('q'):
+				break
 		else:
 			break
