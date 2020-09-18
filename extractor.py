a3db4555a7b52ccc10d211b6c43f267c11c20043
NguyenVanThanhHust <nguyen.van.thanh.hust@gmail.com>
2020-09-18 11:24:03 +0700

    fix mistake: K_inv -> K

--- extractor.py
+++ extractor.py
@@ -1,44 +1,71 @@
 import cv2
 import numpy as np
 from skimage.measure import ransac
-from skimage.transform import FundamentalMatrixTransform
+from skimage.transform import FundamentalMatrixTransform, EssentialMatrixTransform
+np.set_printoptions(suppress=True)
+
+def add_one(x):
+    return np.concatenate([x, np.ones((x.shape[0], 1))], axis=1)
 
 class Extractor(object):
-	GX = 16//2
-	GY = 16//2
-	def __init__(self):
-		self.orb = cv2.ORB_create(100)
-		self.bf = cv2.BFMatcher(cv2.NORM_HAMMING)
-		self.last = None
+    # GX = 16//2
+    # GY = 12//2
+    def __init__(self, K):
 
-	def extract(self, img):
-		#detection
-		feats = cv2.goodFeaturesToTrack(np.mean(img, axis = 2).astype(np.uint8), 3000, qualityLevel = 0.01, minDistance = 3)
-		
-		# extraction
-		kps = [cv2.KeyPoint(x = f[0][0], y = f[0][1], _size = 20) for f in feats]
-		kps, des = self.orb.compute(img, kps)
+        self.orb = cv2.ORB_create()
+        self.bf = cv2.BFMatcher(cv2.NORM_HAMMING)
+        self.last = None
+        self.K = K
+        self.Kinv = np.linalg.inv(self.K)
 
-		# matching
-		matches = None
-		self.last = {'kps': kps, 'des': des}
-		ret = []
-		if self.last is not None:
-			matches = self.bf.knnMatch(des, self.last['des'], k = 2)
-			for m, n in matches:
-				if m.distance < 0.75 * n.distance:
-					kp1 = kps[m.queryIdx].pt
-					kp2 = self.last['kps'][m.trainIdx].pt
-					ret.append((kp1, kp2))
+    def normalize(self, pts):
+        return np.dot(self.Kinv, add_one(pts).T).T[:, 0:2]
 
-		# filter
-		if len(ret) > 0:
-			ret = np.asarray(ret)
-			model, inliners = ransac((ret[:, 0], ret[:, 1]), 
-										FundamentalMatrixTransform, 
-										min_samples = 8, 
-										residual_threshold = 0.01, 
-										max_trials = 100)
-			ret = ret[inliners]
-		self.last = {'kps': kps, 'des': des}
-		return ret
+    def denormalize(self, pt):
+        ret = np.dot(self.K, np.array([pt[0], pt[1], 1.0]))
+
+        # print(ret)
+        return int(round(ret[0])), int(round(ret[1]))
+
+    def extract(self, img):
+        #detection
+        feats = cv2.goodFeaturesToTrack(np.mean(img, axis = 2).astype(np.uint8), 3000, qualityLevel = 0.01, minDistance = 3)
+        
+        # extraction
+        kps = [cv2.KeyPoint(x = f[0][0], y = f[0][1], _size = 20) for f in feats]
+        kps, des = self.orb.compute(img, kps)
+        print(kps)
+        # matching
+        matches = None
+        self.last = {'kps': kps, 'des': des}
+        ret = []
+        if self.last is not None:
+            matches = self.bf.knnMatch(des, self.last['des'], k = 2)
+            for m, n in matches:
+                if m.distance < 0.75 * n.distance:
+                    kp1 = kps[m.queryIdx].pt
+                    kp2 = self.last['kps'][m.trainIdx].pt
+                    print(kp1, kp2)
+                    ret.append((kp1, kp2))
+
+        
+        # filter
+        if len(ret) > 0:
+            ret = np.asarray(ret)
+            # normalize coordes, subtract to move to 
+            ret[:, 0, :] = self.normalize(ret[:, 0, :])
+            ret[:, 1, :] = self.normalize(ret[:, 1, :])
+
+            model, inliners = ransac((ret[:, 0], ret[:, 1]), 
+                                        # FundamentalMatrixTransform, 
+                                        EssentialMatrixTransform, 
+                                        min_samples = 8, 
+                                        residual_threshold = 0.01, 
+                                        max_trials = 100)
+            ret = ret[inliners]
+
+            s, v, d = np.linalg.svd(model.params)
+            f_est = np.sqrt(2)/((v[0] + v[1])/2)
+
+        self.last = {'kps': kps, 'des': des}
+        return ret
--- slam.py
+++ slam.py
@@ -3,33 +3,38 @@ import time
 import numpy as np
 from extractor import Extractor
 
-W = 960
-H = 540
+W = 1280 # 1920 // 2
+H = 720 # 1080 // 2
 
-fe = Extractor()
+F = 282
+K = np.array([[F, 0, W//2], [0, F, H//2], [0, 0, 1]])
+fe = Extractor(K)
+
 def process_frame(img):
-	img = cv2.resize(img, (960, 540))
-	# find the keypoints with ORB
-	matches = fe.extract(img)
-	for p1, p2 in matches:
-		u1, v1 = map(lambda x: int(round(x)), p1)
-		u2, v2 = map(lambda x: int(round(x)), p2)
-		cv2.line(img, (u1, v1), (u2, v2), (0, 255, 0), 1)
-		# cv2.circle(img, (u,v), 5, (0, 255, 0), 1)
-			
-	# filter
+    img = cv2.resize(img, (960, 540))
+    h, w, c = img.shape
+    # find the keypoints with ORB
+    matches = fe.extract(img)
+    for p1, p2 in matches:
+        u1, v1 = fe.denormalize(p1)
+        u2, v2 = fe.denormalize(p2)
 
-	cv2.imshow("result", img)
-	cv2.waitKey(1)
+        cv2.line(img, (u1, v1), (u2, v2), (0, 255, 255), 1)
+        cv2.circle(img, (u1,v1), 5, (0, 255, 0), 1)
+            
+    # filter
+
+    cv2.imshow("result", img)
+    cv2.waitKey(1)
 
 if __name__ == '__main__':
-	cap = cv2.VideoCapture("test.mp4")
-	while cap.isOpened():
-		ret , frame = cap.read()
-		if ret == True:
-			process_frame(frame)
-			# Press Q on keyboard to  exit
-			if cv2.waitKey(1) & 0xFF == ord('q'):
-				break
-		else:
-			break
+    cap = cv2.VideoCapture("test.mp4")
+    while cap.isOpened():
+        ret , frame = cap.read()
+        if ret == True:
+            process_frame(frame)
+            # Press Q on keyboard to  exit
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        else:
+            break
