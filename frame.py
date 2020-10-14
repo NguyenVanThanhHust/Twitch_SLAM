500d1f25718a5b6a3aa74de9d0038e2401d7e8cb
NguyenVanThanhHust <nguyen.van.thanh.hust@gmail.com>
2020-10-14 22:51:43 +0700

    add pangolin 3d visualization

--- frame.py
+++ frame.py
@@ -1,112 +1,100 @@
 import cv2
 import numpy as np
-from skimage.measure import ransac
-from skimage.transform import FundamentalMatrixTransform, EssentialMatrixTransform
 np.set_printoptions(suppress=True)
 
-# def add_one(x):
-    # return np.concatenate([x, np.ones((x.shape[0], 1))], axis=1)
-def add_one(x):
-    if len(x.shape) == 1:
-        return np.concatenate([x,np.array([1.0])], axis=0)
-    else:
-        one_matrix = np.ones((x.shape[0], 1))
-        return np.concatenate([x, one_matrix], axis=1)
+from skimage.measure import ransac
+from skimage.transform import FundamentalMatrixTransform
+from skimage.transform import EssentialMatrixTransform
 
-def extractRt(EssentialMatrix):
-    E = EssentialMatrix
-    U, w, Vt = np.linalg.svd(E)
+# turn [[x,y]] -> [[x,y,1]]
+def add_ones(x):
+  return np.concatenate([x, np.ones((x.shape[0], 1))], axis=1)
 
-    # assert np.linalg.det(U) > 0, "check det of U"
-    if np.linalg.det(U) < 0:
-        U *= -1.0
-    if np.linalg.det(Vt) < 0:
-        Vt *= -1.0
-    W = np.mat([[0, -1, 0], [1, 0, 0], [0, 0, 1]], dtype=float)
-    R = np.dot(np.dot(U, W), Vt)
-    if np.sum(R.diagonal()) < 0:
-        R = np.dot(np.dot(U, W.T), Vt)
-    t = U[:, 2]
-    Rt = np.concatenate([R, t.reshape(3, 1)], axis=1)
-    print(Rt.shape)
-    ret = np.eye(4)
-    ret[:3, :3] = R
-    ret[:3, 3] = t
-    return ret
+IRt = np.eye(4)
 
-def normalize(Kinv, pts):
-    pts_add1 = add_one(pts)
-    return np.dot(Kinv, pts_add1.T).T[:, 0:2]
-
-def denormalize(K, pt):
-    ret = np.dot(K, np.array([pt[0], pt[1], 1.0]))
-    ret /= ret[2]
-    return int(round(ret[0])), int(round(ret[1]))
+# pose
+def extractRt(E):
+  W = np.mat([[0,-1,0],[1,0,0],[0,0,1]],dtype=float)
+  U,d,Vt = np.linalg.svd(E)
+  assert np.linalg.det(U) > 0
+  if np.linalg.det(Vt) < 0:
+    Vt *= -1.0
+  R = np.dot(np.dot(U, W), Vt)
+  if np.sum(R.diagonal()) < 0:
+    R = np.dot(np.dot(U, W.T), Vt)
+  t = U[:, 2]
+  ret = np.eye(4)
+  ret[:3, :3] = R
+  ret[:3, 3] = t
+  return ret
 
 def extract(img):
-    pts = cv2.goodFeaturesToTrack(np.mean(img, axis = 2).astype(np.uint8), 3000, qualityLevel = 0.01, minDistance = 3)
-    orb = cv2.ORB_create()
-    # extraction
-    kps = [cv2.KeyPoint(x = f[0][0], y = f[0][1], _size = 20) for f in pts]
-    kps, des = orb.compute(img, kps)
+  orb = cv2.ORB_create()
+  # detection
+  pts = cv2.goodFeaturesToTrack(np.mean(img, axis=2).astype(np.uint8), 3000, qualityLevel=0.01, minDistance=3)
 
-    return np.array([(kp.pt[0], kp.pt[1]) for kp in kps]), des
+  # extraction
+  kps = [cv2.KeyPoint(x=f[0][0], y=f[0][1], _size=20) for f in pts]
+  kps, des = orb.compute(img, kps)
+
+  # return pts and des
+  return np.array([(kp.pt[0], kp.pt[1]) for kp in kps]), des
+
+def normalize(Kinv, pts):
+  return np.dot(Kinv, add_ones(pts).T).T[:, 0:2]
+
+def denormalize(K, pt):
+  ret = np.dot(K, np.array([pt[0], pt[1], 1.0]))
+  ret /= ret[2]
+  return int(round(ret[0])), int(round(ret[1]))
 
 def match_frames(f1, f2):
-    #detection
-    bf = cv2.BFMatcher(cv2.NORM_HAMMING)
-    matches = bf.knnMatch(f1.des, f2.des, k = 2)
-    
-    ret = []
-    idx1, idx2 = [], []
-    for m, n in matches:
-        if m.distance < 0.75 * n.distance:
-            idx1.append(m.queryIdx)
-            idx2.append(m.trainIdx)
-            p1 = f1.pts[m.queryIdx]
-            p2 = f2.pts[m.trainIdx]
-            ret.append((p1, p2))
+  bf = cv2.BFMatcher(cv2.NORM_HAMMING)
+  matches = bf.knnMatch(f1.des, f2.des, k=2)
 
-    idx1 = np.array(idx1)
-    idx2 = np.array(idx2)
-    # filter
-    assert len(ret) >= 8
-    ret = np.asarray(ret)
-    model, inliners = ransac((ret[:, 0], ret[:, 1]), 
-                                # FundamentalMatrixTransform, 
-                                EssentialMatrixTransform, 
-                                min_samples = 8, 
-                                residual_threshold = 0.01, 
-                                max_trials = 100)
-    # ret = ret[inliners]
-    Rt = extractRt(model.params)
-    idx1 = idx1[inliners]
-    idx2 = idx2[inliners]
+  # Lowe's ratio test
+  ret = []
+  idx1,idx2 = [], []
 
-    return idx1, idx2, ret, Rt
+  for m,n in matches:
+    if m.distance < 0.75*n.distance:
+      # keep around indices
+      idx1.append(m.queryIdx)
+      idx2.append(m.trainIdx)
+
+      p1 = f1.pts[m.queryIdx]
+      p2 = f2.pts[m.trainIdx]
+      ret.append((p1, p2))
+  assert len(ret) >= 8
+  ret = np.array(ret)
+  idx1 = np.array(idx1)
+  idx2 = np.array(idx2)
+
+  # fit matrix
+  model, inliers = ransac((ret[:, 0], ret[:, 1]),
+                          EssentialMatrixTransform,
+                          #FundamentalMatrixTransform,
+                          min_samples=8,
+                          #residual_threshold=1,
+                          residual_threshold=0.005,
+                          max_trials=200)
+  print(sum(inliers), len(inliers))
+
+  # ignore outliers
+  Rt = extractRt(model.params)
+
+  # return
+  return idx1[inliers], idx2[inliers], Rt
 
 class Frame(object):
-    def __init__(self, mapp, img, K, pose=np.eye(4)):
-        self.K = K
-        self.Kinv = np.linalg.inv(self.K)
+  def __init__(self, mapp, img, K):
+    self.K = K
+    self.Kinv = np.linalg.inv(self.K)
+    self.pose = IRt
 
-        pts, self.des = extract(img)
-        self.pts = normalize(self.Kinv, pts)
-        self.pose = pose
-        self.id = len(mapp.frames)
-        mapp.frames.append(self)
-        
-class Extractor(object):
-    # GX = 16//2
-    # GY = 12//2
-    def __init__(self, K):
+    pts, self.des = extract(img)
+    self.pts = normalize(self.Kinv, pts)
 
-        self.orb = cv2.ORB_create()
-        self.bf = cv2.BFMatcher(cv2.NORM_HAMMING)
-        self.last = None
-        self.K = K
-        self.Kinv = np.linalg.inv(self.K)
-
-        # print(ret)
-        return int(round(ret[0])), int(round(ret[1]))
+    self.id = len(mapp.frames)
+    mapp.frames.append(self)
 
--- slam.py
+++ slam.py
@@ -1,100 +1,150 @@
-import cv2
+#!/usr/bin/env python3
 import time
+import cv2
+from frame import Frame, denormalize, match_frames, IRt
 import numpy as np
-from extractor import Extractor
-from frame import Frame, normalize, denormalize, match_frames
 import g2o
 
+import OpenGL.GL as gl
+import pangolin
 
-# camera instrinsics
-W = 1280 # 1920 // 2
-H = 720 # 1080 // 2
+# camera intrinsics
+W, H = 1920//2, 1080//2
 
-F = 250
-K = np.array([[F, 0, W//2], [0, F, H//2], [0, 0, 1]])
-extractor = Extractor(K)
-IRt = np.zeros((3, 4)) # identity trianglation
-IRt[:, :3] = np.eye(3)
+F = 270
+K = np.array([[F,0,W//2],[0,F,H//2],[0,0,1]])
 
-frames = []
-points = []
-
-
-
+from multiprocessing import Process, Queue
 
 class Map(object):
-    def __init__(self):
-        self.frames = []
-        self.points = []
-    def display_map(self, ):
-        for f in self.frames:
-            print(f.pose)
+  def __init__(self):
+    self.frames = []
+    self.points = []
+    self.state = None
+    self.q = Queue()
+
+    p = Process(target=self.viewer_thread, args=(self.q,))
+    p.daemon = True
+    p.start()
+
+  def viewer_thread(self, q):
+    self.viewer_init()
+    while 1:
+      self.viewer_refresh(q)
+
+  def viewer_init(self):
+    pangolin.CreateWindowAndBind('Main', 640, 480)
+    gl.glEnable(gl.GL_DEPTH_TEST)
+
+    self.scam = pangolin.OpenGlRenderState(
+      pangolin.ProjectionMatrix(640, 480, 420, 420, 320, 240, 0.2, 100),
+      pangolin.ModelViewLookAt(-2, 2, -2, 0, 0, 0, pangolin.AxisDirection.AxisY))
+    self.handler = pangolin.Handler3D(self.scam)
+
+    # Create Interactive View in window
+    self.dcam = pangolin.CreateDisplay()
+    self.dcam.SetBounds(0.0, 1.0, 0.0, 1.0, -640.0/480.0)
+    self.dcam.SetHandler(self.handler)
+
+  def viewer_refresh(self, q):
+    if self.state is None or not q.empty():
+      self.state = q.get()
+
+    # turn state into points
+    ppts = np.array([d[:3, 3] for d in self.state[0]])
+    spts = np.array(self.state[1])
+
+    gl.glClear(gl.GL_COLOR_BUFFER_BIT | gl.GL_DEPTH_BUFFER_BIT)
+    gl.glClearColor(1.0, 1.0, 1.0, 1.0)
+    self.dcam.Activate(self.scam)
+
+    gl.glPointSize(10)
+    gl.glColor3f(0.0, 1.0, 0.0)
+    pangolin.DrawPoints(ppts)
+
+    gl.glPointSize(2)
+    gl.glColor3f(0.0, 1.0, 0.0)
+    pangolin.DrawPoints(spts)
+
+    pangolin.FinishFrame()
+
+  def display(self):
+    poses, pts = [], []
+    for f in self.frames:
+      poses.append(f.pose)
+    for p in self.points:
+      pts.append(p.pt)
+    self.q.put((poses, pts))
+
+# main classes
+#disp = Display(W, H)
 mapp = Map()
 
 class Point(object):
-    def __init__(self, mapp, loc):
-        self.xyz = loc
-        self.frames = []
-        self.id = len(mapp.points)
-        self.idxs = []
-        mapp.points.append(self)
+  # A Point is a 3-D point in the world
+  # Each Point is observed in multiple Frames
 
-    def add_observation(self, frame, idx):
-        self.frames.append(frame)
-        self.idxs.append(idx)
+  def __init__(self, mapp, loc):
+    self.pt = loc
+    self.frames = []
+    self.idxs = []
+    
+    self.id = len(mapp.points)
+    mapp.points.append(self)
 
+  def add_observation(self, frame, idx):
+    self.frames.append(frame)
+    self.idxs.append(idx)
 
 def triangulate(pose1, pose2, pts1, pts2):
-    return cv2.triangulatePoints(pose1[:3], pose2[:3], pts1.T, pts2.T).T
+  return cv2.triangulatePoints(pose1[:3], pose2[:3], pts1.T, pts2.T).T
 
 def process_frame(img):
-    img = cv2.resize(img, (960, 540))
-    frame = Frame(mapp, img,K)
-    if frame.id == 0 :
-        return
+  img = cv2.resize(img, (W,H))
+  frame = Frame(mapp, img, K)
+  if frame.id == 0:
+    return
 
-    f1 = mapp.frames[-1]
-    f2 = mapp.frames[-2]
-    # idx1, idx2, pts, Rt = match_frames(frames[-1], frames[-2])
-    idx1, idx2, pts, Rt = match_frames(f1, f2)
-    f1.pose = np.dot(Rt, f2.pose)
-    pts4d = triangulate(f1.pose, f2.pose, f1.pts[idx1], f2.pts[idx2])
+  f1 = mapp.frames[-1]
+  f2 = mapp.frames[-2]
 
-    # reject pts without enough "parallax"
-    good_pts4d = np.abs(pts4d[:, 3]) > 0.005
-    print(len(good_pts4d))
-    pts4d = pts4d[good_pts4d]
-    pts4d /= pts4d[:, 3:]
+  idx1, idx2, Rt = match_frames(f1, f2)
+  f1.pose = np.dot(Rt, f2.pose)
 
-    # reject pts behind the camera
-    good_pts4d = pts4d[:, 2] > 0.005
-    pts4d = pts4d[good_pts4d]
+  # homogeneous 3-D coords
+  pts4d = triangulate(f1.pose, f2.pose, f1.pts[idx1], f2.pts[idx2])
+  pts4d /= pts4d[:, 3:]
 
-    for idx, pt in enumerate(pts4d):
-        pt = Point(mapp, pt)
-        pt.add_observation(f1, idx1[idx])
-        pt.add_observation(f2, idx2[idx])
-        points.append(pt)
+  # reject pts without enough "parallax" (this right?)
+  # reject points behind the camera
+  good_pts4d = (np.abs(pts4d[:, 3]) > 0.005) & (pts4d[:, 2] > 0)
 
-    for p1, p2 in zip(f1.pts[idx1], f2.pts[idx2]):
-        u1, v1 = denormalize(K, p1)
-        u2, v2 = denormalize(K, p2)
+  for i,p in enumerate(pts4d):
+    if not good_pts4d[i]:
+      continue
+    pt = Point(mapp, p)
+    pt.add_observation(f1, idx1[i])
+    pt.add_observation(f2, idx2[i])
 
-        cv2.line(img, (u1, v1), (u2, v2), (0, 255, 255), 1)
-        cv2.circle(img, (u1,v1), 5, (0, 255, 0), 1)
-    mapp.display_map()            
-    cv2.imshow("result", img)
-    cv2.waitKey(1)
+  for pt1, pt2 in zip(f1.pts[idx1], f2.pts[idx2]):
+    u1, v1 = denormalize(K, pt1)
+    u2, v2 = denormalize(K, pt2)
+    cv2.circle(img, (u1, v1), color=(0,255,0), radius=3)
+    cv2.line(img, (u1, v1), (u2, v2), color=(255,0,0))
+  cv2.imshow("result", img)
+  cv2.waitKey(1)
 
-if __name__ == '__main__':
-    cap = cv2.VideoCapture("test.mp4")
-    while cap.isOpened():
-        ret , frame = cap.read()
-        if ret == True:
-            process_frame(frame)
-            # Press Q on keyboard to  exit
-            k = cv2.waitKey(1)
-            if k == 27:
-                break
-        else:
-            break
+  # 3-D
+  mapp.display()
+
+if __name__ == "__main__":
+  cap = cv2.VideoCapture("test.mp4")
+
+  while cap.isOpened():
+    ret, frame = cap.read()
+    if ret == True:
+      process_frame(frame)
+    else:
+      break
+
+
